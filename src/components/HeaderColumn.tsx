563b5c4b2c2cbdb1a0f8bc5191e38eb72ef7faff
mungnxv7 <mungnxhnv7@gmail.com>
2024-08-10 23:52:23 +0700

    write logic rename title column

--- src/components/HeaderColumn.tsx
+++ src/components/HeaderColumn.tsx
@@ -1,4 +1,6 @@
+import { ChangeEvent, useState } from "react";
 import ButtonAdd from "./ButtonAdd";
+import { useDispatch } from "react-redux";
 
 interface HeaderColumnProps {
   title: string;
@@ -6,17 +8,44 @@ interface HeaderColumnProps {
   onClick: () => void;
 }
 
-const HeaderColumn = ({
-  title,
-  quantityColumn,
-  onClick,
-}: HeaderColumnProps) => {
+const HeaderColumn = ({ title, quantityColumn, onClick }: HeaderColumnProps) => {
+  const dispatch = useDispatch();
+  const [isEditTitle, setIsEditTitle] = useState<boolean>(false);
+  const [textTitle, setTextTitle] = useState<string>("");
+  const onSetEditTitle = () => {
+    setIsEditTitle(true);
+  };
+
+  const onBlurInputTitle = () => {
+    setIsEditTitle(false);
+  };
+
+  const onChangeInputTitle = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setTextTitle(value);
+  };
+
   return (
     <div className="flex items-center flex-shrink-0 h-10 px-2">
-      <span className="block text-sm font-semibold">{title}</span>
-      <span className="flex items-center justify-center w-5 h-5 ml-2 text-sm font-semibold text-indigo-500 bg-white rounded bg-opacity-30">
-        {quantityColumn}
-      </span>
+      <div>
+        {isEditTitle ? (
+          <input
+            name="title"
+            className="h-8 rounded-md p-2 bg-transparent outline outline-2 outline-blue-500"
+            onBlur={onBlurInputTitle}
+            onChange={onChangeInputTitle}
+          />
+        ) : (
+          <>
+            <span className="text-sm font-semibold inline-flex cursor-pointer" onClick={onSetEditTitle}>
+              {title}
+            </span>
+            <span className="inline-flex items-center justify-center w-5 h-5 ml-2 text-sm font-semibold text-indigo-500 bg-white rounded bg-opacity-30">
+              {quantityColumn}
+            </span>
+          </>
+        )}
+      </div>
       <ButtonAdd onClick={onClick} />
     </div>
   );
